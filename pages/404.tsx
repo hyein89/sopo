c6bf82831d500e2dec356a8a0364c0d4722df201
hyein89 <bihontai@gmail.com>
2025-07-26 18:51:08 +0700

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+// pages/404.tsx
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: "center", padding: "4rem" }}>
+      <h1>🚫 404</h1>
+      <p>URL tujuan tidak ditemukan. Mohon sertakan parameter <code>?url=</code>.</p>
+    </div>
+  );
+}
